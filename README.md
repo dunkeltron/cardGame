8958761acad269bf5b7facbdd0acff4fb735b368
Ian Dunkelberg <idunkelberg@gmail.com>
2018-10-11 13:26:19 -0700

    added combat functions (takeDamage and calcDamage)

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # It's heavily inspired by slay the spire.
 #10/9 TO DO!
 1. The toolTips need some love currently the tooltip hovers on the leeft somewhere and that's anti-bueno.
-2. The object (cards, players, characters, drawPile, exhaustPile, discardPile relations need to be ironed out and coded up.
+2. The object (cards, players, characters, drawPile, exhaustPile, discardPile) relations need to be ironed out and coded up.
 3. The turn structure needs to get nailed down and implemeneted.
 4. the Cards
+# 10/11 TO DO!
+1. Haven't worked on the tool tips.
+2. Combat turn logic.
+3. character interaction(damage).
--- assets/javascript/character.js
+++ assets/javascript/character.js
@@ -6,13 +6,49 @@ class Character{
         this.mHP = MHP;
         this.cHP=CHP;
         this.buffs=buffsArr
+        this.block =0;
     }
     //manipulators
+    calcDamage(dam,buffs){
+        var totalDam=dam;
+        var multiTotal=1;
+        buffs.forEach(element => {
+            if(element.isDamage() && element.isAdditive()){
+                totalDam+=element.modifier(dam);
+            }            
+        });
+        buffs.forEach(element => {
+            if(element.isDamage() && element.isMultiplicative()){
+                multiTotal*=element.modifier(multiTotal);
+            }            
+        });
+        return totalDam*multiTotal;
+    }
+    takeDamage(dam){
+        this.cHP= this.cHp+this.block - dam;
+    }
+    addBlock(blockNum){
+        this.block=this.block+blockNum;
+    }
     addBuff(newBuff){
         this.buffs.push(newBuff);
     }
     removeBuff(buffTarg){
         this.removeBuff(buffTarg);
+    }
+    iterateBuffs(){
+        this.buffs.forEach(element => {
+            element.iterateBuff();
+        });
+    }
+    
+    endTurnCycle(){
+        
+        //this.iterateBuffs();
+    }
+    startTurnCycle(){
+        this.block=0;
+        resetEnergy();
     }
     //getters
     getAffiliation(){
